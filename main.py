4108b86791c88d258f93735ca40a17379f4e678c
martinstolu <tolu.martins.a@gmail.com>
2025-06-26 23:17:07 +0200

    updating files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -8,5 +10,7 @@ def server():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
